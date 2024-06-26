214b9a916fcf89bc055f1766b97d844a244dbd46
Harikrishnan <haripdm9446718962@gmail.com>
2024-06-27 00:36:37 +0530

    doesnot display the products

--- venv/Scripts/RED_STORE/products/urls.py
+++ venv/Scripts/RED_STORE/products/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-    path('product/', views.product, name='product'),
+    path('product/', views.list_product, name='product'),
     path('product_detail_content/', views.detail_product, name='detail_product'),
     path('cart_page/', views.cart, name='cart_page'),
     path('account/', views.account, name='account'),
--- venv/Scripts/RED_STORE/products/views.py
+++ venv/Scripts/RED_STORE/products/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 
 def index(request):
     return render(request,'layout.html')
 
-def product(request):
-    return render(request,'product_page_content.html')
+def list_product(request):
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request,'product_page_content.html',context)
 
 def detail_product(request):
     return render(request,'product_details.html')
