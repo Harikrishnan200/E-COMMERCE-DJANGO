037e741608a20b4282e34f26deef35c78e45d93e
Harikrishnan <haripdm9446718962@gmail.com>
2024-06-27 17:28:26 +0530

    latest products and featured products can be added successfully

--- venv/Scripts/RED_STORE/products/views.py
+++ venv/Scripts/RED_STORE/products/views.py
@@ -4,7 +4,18 @@ from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
-    return render(request,'layout.html')
+    featured_products = Product.objects.order_by('-priority')[:4]  # To fetch firts 4 products with higher priority (decenting order)
+    latest_products = Product.objects.order_by('-id')[:4]    # To fetch firts 4 last added products (ie the products with higher id)
+    
+    context = {
+        'featured_products': featured_products,
+        'latest_products': latest_products
+    }
+    return render(request,'layout.html',context)   
+#The context dictionary you create in the index view function, containing the keys 
+# 'featured_products' and 'latest_products', is passed to the layout.html template when rendering. These keys, and their associated 
+# values, will be available only within the scope of that specific template rendering. If layout.html includes other templates or if it
+# extends from a base template, those keys will also be available in those included or extended templates during that rendering process.
 
 def list_product(request):
     page = 1
@@ -12,7 +23,8 @@ def list_product(request):
     if 'page' in request.GET:
         page = request.GET.get('page')
 
-    products = Product.objects.all()
+    # products = Product.objects.all()
+    products = Product.objects.order_by('-priority')   # To fetech the products with higher priority
     paginator = Paginator(products, 3)
     products = paginator.get_page(page)
     context = {
