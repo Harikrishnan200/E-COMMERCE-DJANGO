6da6754260fcf8d57f46ef2adbac67887fcf88c0
Harikrishnan <haripdm9446718962@gmail.com>
2024-07-04 10:53:49 +0530

    some changes made

--- venv/Scripts/RED_STORE/products/admin.py
+++ venv/Scripts/RED_STORE/products/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.db import models  # Import the models module
 from .models import Product,Category,Size,ProductSize,SubImage
+from .models import Size, ProductSize
 
 # Register your models here.
 
@@ -24,8 +25,6 @@ class ProductAdmin(admin.ModelAdmin):
 
 # admin.py
 
-from django.contrib import admin
-from .models import Size, ProductSize
 
 class SizeAdmin(admin.ModelAdmin):
     list_display = ['size', 'created_at', 'updated_at']
@@ -46,6 +45,11 @@ admin.site.register(ProductSize, ProductSizeAdmin)
 
 # list_display: Updated to include 'get_model', which is a method that retrieves the model of the product associated with each ProductSize.
 # get_model Method: Defined within ProductSizeAdmin, it accesses obj.product.model to fetch the model field of the related Product. The obj here represents each instance of ProductSize in the queryset.
+
+# get_model: This method retrieves the model attribute from the related product object. The obj parameter represents the current instance of ProductSize being processed in the admin interface.
+# obj.product.model: Accesses the model attribute of the product related to the ProductSize instance.
+
+# short_description: Provides a human-readable name for the column in the admin interface. In this case, it labels the column as "Model".
 
 
 
